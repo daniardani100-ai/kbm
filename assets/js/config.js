feb353374d9d1c792278032a9c855b89c9fad27d
daniardani100-ai <daniardani100@gmail.com>
2026-07-04 21:27:11 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbyv80iAI898NM7o0_EJ6aDGP06czWgbukW-XDZr-tWlCKYLSwO0Pn4L2XG0LPAmqphQpA/exec";
+  "https://script.google.com/macros/s/AKfycbywINo7lQrLhySRBOLml2SICQvbMBPBYJ4W8M09qco441oUOgUdbBEIZT74OeQhcIe6yQ/exec";
